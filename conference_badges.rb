86f2ea9d88196699d2f432e4a24d70aabe77778e
Luis Hong <luishong50@gmail.com>
2019-03-26 23:49:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+   "Hello, my name is "+ name + "."
+end
+
+
+
+
+def batch_badge_creator(attendees) #use for loop, maybe, to access each element in array
+  # badges
+
+attendees.do |element|#????
+  
+end
+ 
+
+ 
+ 
+ 
+def assign_rooms(attendees)
+  room_assignments
+end
+
+
+
+
+
+def printer(attendees)
+ 
+end
